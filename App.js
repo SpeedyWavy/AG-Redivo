83afd9cc48f27e58c7d463a22bb40cf65f773703
SpeedyWavy <46286725+SpeedyWavy@users.noreply.github.com>
2026-04-09 15:56:45 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import AddTaskScreen from "./screens/AddTaskScreen";
+import TarefasScreen from "./screens/TarefasScreen";
+
+export default function App() {
+  const [screen, setScreen] = useState("list");
+
+  const handleGoToAdd = () => setScreen("add");
+  const handleGoToList = () => setScreen("list");
+
+  return (
+    <>
+      {screen === "list" ? (
+        <TarefasScreen onAddPress={handleGoToAdd} />
+      ) : (
+        <AddTaskScreen onBackPress={handleGoToList} onSavePress={handleGoToList} />
+      )}
+      <StatusBar style="auto" />
+    </>
+  );
+}
